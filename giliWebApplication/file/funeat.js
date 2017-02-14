7a93d95f5e24bcbbb01e2a792d68f012d01a5312
adarashaham <shaham.adara@gmail.com>
2017-02-14 20:45:33 +0200

    initial commit

---
+++ giliWebApplication/file/funeat.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#ddmenu').load('funeat.html');
+});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
